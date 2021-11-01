a60786b4d99d5ef4d48ca3ae511e896b76d854ac
m-m-black <morganblack1988@gmail.com>
2021-11-01 12:39:02 +1100

    Add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={styles.appContainer}
+      value={input}
+      onChange={(e) => {
+        setInput(e.target.value);
+      }}
+    >
+      <textarea autoFocus style={styles.textArea} />
+      <div style={styles.markdownArea}>
+        <ReactMarkdown
+          children={input}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  style={docco}
+                  language={match[1]}
+                  PreTag='div'
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
+
+const styles = {
+  appContainer: {
+    display: 'flex',
+    height: '100vh',
+  },
+
+  textArea: {
+    width: '50%',
+    height: '100%',
+    padding: 20,
+    fontSize: '1.5rem',
+    outline: 'none',
+  },
+
+  markdownArea: {
+    width: '50%',
+    height: '100%',
+    padding: 20,
+    outline: 'none',
+  },
+};
 
 export default App;
